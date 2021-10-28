a4b087600e8bad11c64a9fb5cc2ca13fdf7f4ea1
Aleksandr <aleksberezkin68@mail.ru>
2021-10-28 13:30:50 +0900

    flags_berezkin
    flags

---
+++ app/src/main/java/com/example/lilst_view_berezkin/Colombia.java
@@ -0,0 +1,14 @@
+package com.example.lilst_view_berezkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colombia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colombia);
+    }
+}
